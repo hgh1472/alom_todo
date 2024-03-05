c54ddebff669cca5e7aae8366ed3aabd5a24aa3b
GunHa Hwang <hgh1472@naver.com>
2024-03-05 20:59:47 +0900

    refactor: 중복 이메일 예외 체크 로직 컨트롤러로 이동

--- todo/src/main/java/spring/todo/controller/MemberController.java
+++ todo/src/main/java/spring/todo/controller/MemberController.java
@@ -2,9 +2,15 @@ package spring.todo.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import spring.todo.domain.Member;
+import spring.todo.exception.DuplicateException;
+import spring.todo.exception.ErrorConst;
+import spring.todo.exception.ErrorResult;
 import spring.todo.repository.member.MemberDto;
 import spring.todo.service.MemberService;
 
@@ -15,11 +21,22 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(DuplicateException.class)
+    public ErrorResult duplicateExceptionHandler(DuplicateException e) {
+        log.error("[duplicated email]", e);
+        return new ErrorResult(ErrorConst.DUPLICATE_EXCEPTION);
+    }
+
     @PostMapping("/join")
-    public void join(MemberDto memberDto) {
+    public Long join(MemberDto memberDto) {
         log.info("email={}", memberDto.getEmail());
         log.info("password={}", memberDto.getPassword());
         log.info("nickname={}", memberDto.getNickname());
-        memberService.join(memberDto);
+        Long memberId = memberService.join(memberDto);
+        if (memberId == null) {
+            throw new DuplicateException(ErrorConst.DUPLICATE_EXCEPTION.getMessage());
+        }
+        return memberId;
     }
 }
