a7c65126e057a2fecf7ae328ebcd4f56dc53dd94
GunHa Hwang <hgh1472@naver.com>
2024-03-07 16:53:28 +0900

    feat: 권한 검사를 위한 UserDetailsImpl, UserDetailsServiceImpl 구현

---
+++ todo/src/main/java/spring/todo/security/UserDetailsImpl.java
@@ -0,0 +1,65 @@
+package spring.todo.security;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import spring.todo.domain.Member;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Slf4j
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    @Getter
+    private final Member member;
+    private final String password;
+    private final String email;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Authority role = member.getAuthority();
+        String authority = role.getAuthority();
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ todo/src/main/java/spring/todo/security/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package spring.todo.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import spring.todo.domain.Member;
+import spring.todo.repository.member.MemberRepository;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+
+
+    private final MemberRepository memberRepository;
+    @Override
+    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+        long memberId = Long.parseLong(id);
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+        return new UserDetailsImpl(member, member.getPassword(), member.getEmail());
+    }
+
+}
