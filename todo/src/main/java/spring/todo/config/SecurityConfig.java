56b80aa84ec5ee3f5808334819d1d45e9cd84454
GunHa Hwang <hgh1472@naver.com>
2024-03-07 17:00:17 +0900

    refactor: jwt 토큰 검증 추가로 인한 filterChain 수정

--- todo/src/main/java/spring/todo/config/SecurityConfig.java
+++ todo/src/main/java/spring/todo/config/SecurityConfig.java
@@ -1,20 +1,28 @@
 package spring.todo.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import spring.todo.security.*;
 
 /**
  * @Configuration 애노테이션이 @EnableWebSecurity에 포함되어 있다.
  */
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+
     /**
      * PasswordEncoder interface의 구현체가 BCryptPasswordEncoder임을 수동 빈 등록을 통해서 명시한다.
      */
@@ -25,7 +33,17 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(AbstractHttpConfigurer::disable);
-        return httpSecurity.build();
+        return httpSecurity
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/join").permitAll()
+                .requestMatchers("/members/{id}").hasAuthority(Authority.MEMBER.getAuthority())
+                .and()
+                .addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 }
