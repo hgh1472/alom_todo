2c87a6ef81900acde521cc7892a512aadd0615e6
GunHa Hwang <hgh1472@naver.com>
2024-03-07 16:58:55 +0900

    feat: 멤버 정보 API 추가

--- todo/src/main/java/spring/todo/controller/MemberController.java
+++ todo/src/main/java/spring/todo/controller/MemberController.java
@@ -3,13 +3,13 @@ package spring.todo.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import spring.todo.domain.Member;
 import spring.todo.exception.DuplicateException;
 import spring.todo.exception.ErrorConst;
 import spring.todo.exception.ErrorResult;
+import spring.todo.exception.WrongAccessException;
+import spring.todo.repository.member.LoginOutputDto;
 import spring.todo.repository.member.MemberDto;
 import spring.todo.service.MemberService;
 
@@ -27,6 +27,13 @@ public class MemberController {
         return new ErrorResult(ErrorConst.DUPLICATE_EXCEPTION);
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WrongAccessException.class)
+    public ErrorResult wrongAccessExceptionHandler(WrongAccessException e) {
+        log.error("[wrong access]", e);
+        return new ErrorResult(ErrorConst.WRONG_ACCESS_EXCEPTION);
+    }
+
     @PostMapping("/join")
     public Long join(MemberDto memberDto) {
         log.info("email={}", memberDto.getEmail());
@@ -37,5 +44,18 @@ public class MemberController {
             throw new DuplicateException(ErrorConst.DUPLICATE_EXCEPTION.getMessage());
         }
         return memberId;
+    }
+
+    @PostMapping("/members/{id}")
+    public LoginOutputDto memberInfo(@PathVariable String id) {
+        Long memberId = Long.parseLong(id);
+        Member findMember = memberService.getMemberInfo(memberId);
+        if (findMember == null) {
+            throw new WrongAccessException(ErrorConst.WRONG_EXCEPTION.getMessage());
+        }
+        log.info("findMember={}", findMember.toString());
+        LoginOutputDto outputDto = new LoginOutputDto(findMember.getEmail(), findMember.getNickname());
+        log.info("email = {}, nickname={}", outputDto.getEmail(), outputDto.getNickname());
+        return outputDto;
     }
 }
