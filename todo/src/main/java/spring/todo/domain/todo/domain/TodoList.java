1dbaebaa9203fe6442b7c1c23582a7594dd665b9
GunHa Hwang <hgh1472@naver.com>
2024-03-18 23:48:30 +0900

    feat: TodoList 도메인 구현

---
+++ todo/src/main/java/spring/todo/domain/todo/domain/TodoList.java
@@ -0,0 +1,22 @@
+package spring.todo.domain.todo.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import spring.todo.domain.member.domain.Member;
+
+import java.util.List;
+
+@Entity
+@Getter
+public class TodoList {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long code;
+
+    @OneToOne
+    private Member writer;
+
+    @OneToMany
+    @JoinColumn(name = "list_id")
+    private List<Todo> todoList;
+}
