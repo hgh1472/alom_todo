cd2bc5f9a443982652a4ad2411c4836fcc5e110a
GunHa Hwang <hgh1472@naver.com>
2024-03-25 22:40:10 +0900

    [Feat] Todo 리스트 생성 및 조회 기능 구현

---
+++ todo/src/main/java/spring/todo/domain/todo/controller/TodoListController.java
@@ -0,0 +1,42 @@
+package spring.todo.domain.todo.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import spring.todo.domain.member.domain.Member;
+import spring.todo.domain.member.service.MemberService;
+import spring.todo.domain.todo.domain.TodoListInfo;
+import spring.todo.domain.todo.repository.CreateRequestDto;
+import spring.todo.domain.todo.repository.ReadTodoListDto;
+import spring.todo.domain.todo.service.TodoService;
+
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class TodoListController {
+
+    private final MemberService memberService;
+    private final TodoService todoService;
+
+    @PostMapping("/todolist/create")
+    public Long createTodoList(@RequestBody CreateRequestDto createRequestDto) {
+        log.info("[createTodoList] writerId = {}", createRequestDto.getWriterId());
+
+        Member writer = memberService.getMemberInfo(createRequestDto.getWriterId());
+        TodoListInfo todoListInfo = createRequestDto.toEntity(writer);
+        return todoService.registerTodoList(todoListInfo);
+    }
+
+    @PostMapping("/todolist/{code}")
+    public ReadTodoListDto readTodoListInfo(@PathVariable Long code) {
+        TodoListInfo todoListInfo = todoService.readTodoListInfo(code);
+        ReadTodoListDto readTodoListDto = ReadTodoListDto.of(todoListInfo);
+        log.info("[TOdoListController.readTodoListInfo] ReadTodoListDto={}", readTodoListDto);
+        return readTodoListDto;
+    }
+
+}
