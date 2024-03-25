3f634c37e94833565d57fc9c0214c8b3b7d1a7c0
GunHa Hwang <hgh1472@naver.com>
2024-03-25 22:40:53 +0900

    [Refactor] Dto 명 변경에 따른 수정

--- todo/src/main/java/spring/todo/web/controller/LoginController.java
+++ todo/src/main/java/spring/todo/web/controller/LoginController.java
@@ -15,7 +15,7 @@ import spring.todo.exception.EmptyException;
 import spring.todo.exception.ErrorConst;
 import spring.todo.exception.ErrorResult;
 import spring.todo.exception.WrongException;
-import spring.todo.domain.member.repository.LoginDto;
+import spring.todo.domain.member.repository.LoginRequestDto;
 import spring.todo.security.JwtTokenDto;
 import spring.todo.security.JwtTokenProvider;
 import spring.todo.web.service.LoginService;
@@ -44,19 +44,19 @@ public class LoginController {
     private final LoginService loginService;
 
     @PostMapping("/login")
-    public JwtTokenDto login(@Valid @RequestBody LoginDto loginDto, BindingResult bindingResult, HttpServletResponse response) {
+    public JwtTokenDto login(@Valid @RequestBody LoginRequestDto loginRequestDto, BindingResult bindingResult, HttpServletResponse response) {
 
         log.info("[login]");
         if (bindingResult.hasErrors()) {
             throw new EmptyException(ErrorConst.EMPTY_EXCEPTION.getMessage());
         }
 
-        Member loginMember = loginService.login(loginDto.getEmail(), loginDto.getPassword());
+        Member loginMember = loginService.login(loginRequestDto.getEmail(), loginRequestDto.getPassword());
         if (loginMember == null) {
             throw new WrongException(ErrorConst.WRONG_EXCEPTION.getMessage());
         }
         log.info("[loginV2] loginMember = {}", loginMember.toString());
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword());
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginRequestDto.getEmail(), loginRequestDto.getPassword());
         log.info("authenticationToken = {}, {}",authenticationToken.getName(), authenticationToken.getCredentials());
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
         log.info("authorities = {}", authentication.getAuthorities());
