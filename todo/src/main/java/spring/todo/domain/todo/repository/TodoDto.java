989325426eb8f51c66848b626124aeed182055df
GunHa Hwang <hgh1472@naver.com>
2024-03-26 17:36:57 +0900

    [Refactor] @RequestBody를 받기 위해 생성자 애노테이션 추가

--- todo/src/main/java/spring/todo/domain/todo/repository/TodoDto.java
+++ todo/src/main/java/spring/todo/domain/todo/repository/TodoDto.java
@@ -1,14 +1,17 @@
 package spring.todo.domain.todo.repository;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import spring.todo.domain.todo.domain.Todo;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class TodoDto {
 
     private String description;
