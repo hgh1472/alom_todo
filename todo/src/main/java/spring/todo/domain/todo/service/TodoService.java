7c5e5913aa6441393a430431ed8235465242ba8b
GunHa Hwang <hgh1472@naver.com>
2024-03-26 17:38:34 +0900

    [Feat] TodoList 삭제 로직 구현

--- todo/src/main/java/spring/todo/domain/todo/service/TodoService.java
+++ todo/src/main/java/spring/todo/domain/todo/service/TodoService.java
@@ -3,6 +3,7 @@ package spring.todo.domain.todo.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import spring.todo.domain.todo.domain.TodoListInfo;
 import spring.todo.domain.todo.repository.TodoListRepository;
 
@@ -13,6 +14,9 @@ public class TodoService {
     private final TodoListRepository todoListRepository;
 
     public Long registerTodoList(TodoListInfo todoListInfo) {
+        todoListInfo.getTodoList()
+                .stream()
+                .forEach(todo -> todo.setTodoListInfo(todoListInfo));
         Long code = todoListRepository.save(todoListInfo).getCode();
         return code;
     }
@@ -20,5 +24,10 @@ public class TodoService {
     public TodoListInfo readTodoListInfo(Long code) {
         TodoListInfo findTodoListInfo = todoListRepository.findTodoListInfoByCode(code);
         return findTodoListInfo;
+    }
+
+    @Transactional
+    public Long deleteTodoListInfo(Long code) {
+        return todoListRepository.removeTodoListInfoByCode(code);
     }
 }
